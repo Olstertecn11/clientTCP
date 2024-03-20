5249db65335e5f6134e72f462aeb8e8ff9c43d7c
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-03-20 05:20:25 -0600

    client tcp

--- clientServer.py
+++ clientServer.py
@@ -1,5 +1,9 @@
+
 import socket
 import threading
+import json
+import time
+import random
 
 
 class Client:
@@ -7,6 +11,8 @@ class Client:
         self.server_ip = server_ip
         self.server_port = server_port
         self.cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.last_message = ""
+        self.in_collision = False  # Bandera para indicar si está en estado de colisión
 
     def connect(self):
         self.cliente.connect((self.server_ip, self.server_port))
@@ -21,7 +27,14 @@ class Client:
             if mensaje.lower() == 'exit':
                 self.cliente.close()
                 break
-            self.cliente.sendall(mensaje.encode())
+            self.send_message(mensaje)
+
+    def send_message(self, mensaje):
+        timestamp = time.strftime("%H:%M:%S", time.localtime())  # Obtener tiempo actual
+        message_data = {"time": timestamp, "message": mensaje}
+        message_json = json.dumps(message_data)
+        self.cliente.sendall(message_json.encode())
+        self.last_message = message_json  # Guardar el último mensaje enviado
 
     def receive_messages(self):
         while True:
@@ -29,7 +42,24 @@ class Client:
             if not data:
                 print("Se ha perdido la conexión con el servidor.")
                 break
-            print('Respuesta del servidor:', data.decode())
+            response = data.decode()
+            if response == '{"code": "collision"}':
+                if not self.in_collision:  # Evitar bucles infinitos de colisiones
+                    print("Colisión detectada. Reenviando mensaje...")
+                    self.in_collision = True  # Establecer la bandera de colisión
+                    # Espera un número aleatorio de segundos menor a 10
+                    wait_time = random.random() * 10
+                    print(f"Mensaje será enviado dentro de {wait_time} segundos")
+                    time.sleep(int(wait_time))
+                    # Reenviar el último mensaje enviado con un nuevo tiempo
+                    self.send_message_with_new_time()
+                    self.in_collision = False  # Restablecer la bandera de colisión después del reenvío
+            else:
+                print('Respuesta del servidor:', response)
 
-
+    def send_message_with_new_time(self):
+        mensaje_data = json.loads(self.last_message)
+        mensaje_data["time"] = time.strftime("%H:%M:%S", time.localtime())  # Actualizar el tiempo
+        new_message_json = json.dumps(mensaje_data)
+        self.cliente.sendall(new_message_json.encode())  # Enviar mensaje actualizado
 
